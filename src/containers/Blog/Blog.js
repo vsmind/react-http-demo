5588732cc9b6abbe16b22f021e889a0860265ec9
Vitaly <mtgmind@gmail.com>
2018-11-20 23:11:18 +0100

    Handling errors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     };
 
     componentDidMount() {
@@ -25,6 +26,10 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 //console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({error: true});
             });
     }
 
@@ -33,15 +38,18 @@ class Blog extends Component {
     };
 
     render () {
-        const posts = this.state.posts.map(
-            post => {
-                return <Post key={post.id}
-                             title={post.title}
-                             author={post.author}
-                             clicked={() => this.postSelectedHandler(post.id)}
-                />
-            }
-        );
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error){
+            posts = this.state.posts.map(
+                post => {
+                    return <Post key={post.id}
+                                 title={post.title}
+                                 author={post.author}
+                                 clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                }
+            );
+        }
 
         return (
             <div>
